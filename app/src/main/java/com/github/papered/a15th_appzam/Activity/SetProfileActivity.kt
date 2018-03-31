9ab479ab2b73b41c23587087515cd5d34d79f9a7
jungwoochang <woochang4862@gmail.com>
2018-04-01 03:36:03 +0900

    c
    c

--- app/src/main/java/com/github/papered/a15th_appzam/Activity/SetProfileActivity.kt
+++ app/src/main/java/com/github/papered/a15th_appzam/Activity/SetProfileActivity.kt
@@ -20,8 +20,6 @@ class SetProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_profile)
 
-        startActivity(Intent(this, ContentActivity::class.java))
-
         setProfile_img_profile.background = ShapeDrawable(OvalShape())
         setProfile_img_profile.clipToOutline = true
 
