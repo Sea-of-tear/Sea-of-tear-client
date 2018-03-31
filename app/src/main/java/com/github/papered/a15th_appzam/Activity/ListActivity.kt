c66bb3fd461dcf86d15e928b9def73aec7aa1dd1
jungwoochang <woochang4862@gmail.com>
2018-04-01 05:31:52 +0900

    w
    w

---
+++ app/src/main/java/com/github/papered/a15th_appzam/Activity/ListActivity.kt
@@ -0,0 +1,13 @@
+package com.github.papered.a15th_appzam.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.github.papered.a15th_appzam.R
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
