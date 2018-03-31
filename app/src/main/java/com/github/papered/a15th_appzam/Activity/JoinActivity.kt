17c9c9a67c9f263fbd87a9102f62ec01dcbdd9b8
이종현 <dwg7691@gmail.com>
2018-03-31 21:37:17 +0900

    Add retrofit module

--- app/src/main/java/com/github/papered/a15th_appzam/Activity/JoinActivity.kt
+++ app/src/main/java/com/github/papered/a15th_appzam/Activity/JoinActivity.kt
@@ -9,5 +9,7 @@ class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+
     }
 }
--- app/src/main/java/com/github/papered/a15th_appzam/Activity/LoginActivity.kt
+++ app/src/main/java/com/github/papered/a15th_appzam/Activity/LoginActivity.kt
@@ -2,12 +2,32 @@ package com.github.papered.a15th_appzam.Activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.papered.a15th_appzam.Connect.Connector
 import com.github.papered.a15th_appzam.R
+import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        Connector.api.auth(login_edit_id.text.toString(), login_edit_pw.text.toString()).enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>?, response: Response<Void>?) {
+                if (response!!.isSuccessful) {
+                    when (response.code()) {
+
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Void>?, t: Throwable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
     }
 }
