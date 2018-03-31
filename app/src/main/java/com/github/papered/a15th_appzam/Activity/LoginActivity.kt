ffa72ca06858d8bc383955f6fc3cb21bab968be0
jungwoochang <woochang4862@gmail.com>
2018-03-31 20:29:15 +0900

    edit

--- app/src/main/java/com/github/papered/a15th_appzam/Activity/LoginActivity.kt
+++ app/src/main/java/com/github/papered/a15th_appzam/Activity/LoginActivity.kt
@@ -1,13 +1,20 @@
 package com.github.papered.a15th_appzam.Activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.github.papered.a15th_appzam.R
+import kotlinx.android.synthetic.main.activity_join.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        join_btn.setOnClickListener{
+            startActivity(Intent(this, JoinActivity::class.java))
+            finish()
+        }
     }
 }
